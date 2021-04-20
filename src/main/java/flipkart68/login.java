633731c832a4445360fe5867f7e3473aeff57bcb
root <root@ip-172-31-29-17.ec2.internal>
2021-04-20 07:21:24 +0000

    i am not blinder

---
+++ src/main/java/flipkart68/login.java
@@ -0,0 +1,14 @@
+package flipkart68;
+
+/**
+ * Hello world!
+ *
+ */
+public class login
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
---
+++ src/test/java/flipkart68/logintest.java
@@ -0,0 +1,21 @@
+package flipkart68;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class loginTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
+
